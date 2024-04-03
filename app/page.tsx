2737ddfea3b5b1922f8deec25c60ff4863daad6f
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-04-03 17:48:12 +0530

    add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Hero from "@/components/main/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Dev Portfolio</div>
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
+    </main>
   );
 };
