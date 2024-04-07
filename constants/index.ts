ea92d8023f99e7ddbb4ef45f3b6e82c23c26c7eb
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-04-07 14:42:16 +0530

    add skills section

--- constants/index.ts
+++ constants/index.ts
@@ -1,69 +1,9 @@
-export const Skill_data = [
+export const Skill_main = [
     {
-        skill_name: "Html 5",
-        Image: "/html.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Css",
-        Image: "/css.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Java Script",
-        Image: "/js.png",
-        width: 65,
-        height: 65,
-    },
-    {
-        skill_name: "Tailwind Css",
-        Image: "/tailwind.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "React",
-        Image: "/react.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Redux",
-        Image: "/redux.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "React Query",
-        Image: "/reactquery.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Type Script",
-        Image: "/ts.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Next js 14",
-        Image: "/next.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Node js",
-        Image: "/node-js.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Mongo db",
-        Image: "/mongodb.png",
-        width: 40,
-        height: 40,
+        skill_name: "C++",
+        Image: "/cpp.png",
+        width: 60,
+        height: 60,
     },
     {
         skill_name: "Python",
@@ -76,6 +16,31 @@ export const Skill_data = [
         Image: "/linux.png",
         width: 80,
         height: 80,
+    },
+    {
+        skill_name: "AWS",
+        Image: "/AWS.png",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "Docker",
+        Image: "/docker.webp",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "Tensorflow",
+        Image: "/tensorflow.svg",
+        width: 70,
+        height: 70,
+    },
+
+    {
+        skill_name: "Numpy",
+        Image: "/Numpy.png",
+        width: 50,
+        height: 50,
     },
 
 ];
@@ -131,12 +96,6 @@ export const Frontend_skill = [
     {
         skill_name: "React",
         Image: "/react.png",
-        width: 80,
-        height: 80,
-    },
-    {
-        skill_name: "Redux",
-        Image: "/redux.png",
         width: 80,
         height: 80,
     },
@@ -210,37 +169,6 @@ export const Backend_skill = [
         height: 80,
     },
 ];
-
-export const ML = [
-    {
-        skill_name: "AWS",
-        Image: "/AWS.png",
-        width: 70,
-        height: 70,
-    },
-    {
-        skill_name: "Docker",
-        Image: "/docker.webp",
-        width: 70,
-        height: 70,
-    },
-    {
-        skill_name: "Tensorflow",
-        Image: "/tensorflow.svg",
-        width: 70,
-        height: 70,
-    },
-
-    {
-        skill_name: "Numpy",
-        Image: "/Numpy.png",
-        width: 50,
-        height: 50,
-    },
-
-];
-
-
 
 export const Other_skill = [
     {
