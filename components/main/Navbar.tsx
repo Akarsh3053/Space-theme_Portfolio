78e3420bda122cf824e3f8b6ef95d141f7ea7cf3
Akarsh Bajpai <onlyakarsh@gmail.com>
2024-04-15 18:43:24 +0530

    fixed deployment issues

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-            <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+            <div className="w-full h-full flex flex-row items-center justify-between m-auto md:px-[10px]">
                 <a
                     href="#about-me"
                     className="h-auto w-auto flex flex-row items-center"
@@ -17,7 +17,7 @@ const Navbar = () => {
                         width={70}
                         unoptimized
                         height={70}
-                        className="cursor-pointer hover:animate-slowspin"
+                        className="cursor-pointer "
                     />
 
                     <span className="font-bold ml-[10px] hidden md:block text-gray-300">
@@ -26,9 +26,9 @@ const Navbar = () => {
                 </a>
 
                 <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-                    <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+                    <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] sm:gap-1 rounded-full text-gray-200">
                         <a href="#about-me" className="cursor-pointer">
-                            About me
+                            About
                         </a>
                         <a href="#skills" className="cursor-pointer">
                             Skills
@@ -39,9 +39,9 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-row gap-5">
-                    {Socials.map((social) => (
-                        <Link href={social.url}>
+                <div className="md:flex md:flex-row md:gap-5 hidden">
+                    {Socials.map((social, index) => (
+                        <Link href={social.url} key={index}>
                             <Image src={social.src} alt={social.name} key={social.name} width={24} height={24} />
                         </Link>
                     ))}
